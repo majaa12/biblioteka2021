f849eb58b1a80ffa40cd07844e0113c23dddc365
majaa12 <majacol98@gmail.com>
2021-03-01 14:57:05 +0100

    Uveden bibliotekaInterfejs

--- src/biblioteka/Interfejs/BibliotekaInterface.java
+++ src/biblioteka/Interfejs/BibliotekaInterface.java
@@ -1,5 +1,22 @@
 package biblioteka.Interfejs;
 
+import java.util.LinkedList;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
 public interface BibliotekaInterface {
 
+	public void dodajKnjigu(Knjiga knjiga);
+	
+	public void obrisiKnjigu(Knjiga knjiga);
+	
+	public LinkedList<Knjiga> vratiSveKnjige();
+	
+	public LinkedList<Knjiga> pronadjiKnjigu(
+			Autor autor,
+			String isbn,
+			String naslov,
+			String izdavac);
+	
 }
