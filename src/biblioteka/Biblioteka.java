4443f9d7ccb0382b75ecb6782483531afe80c2fe
majaa12 <majacol98@gmail.com>
2021-03-15 15:46:46 +0100

    Dokumentovana klasa Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -4,8 +4,17 @@ import java.util.LinkedList;
 
 import biblioteka.Interfejs.BibliotekaInterface;
 
+/**
+ * Ova klasa implementira interfejs BibliotekaInterface i predstavlja implementaciju preko liste
+ * 
+ * @author Maja Colovic
+ *
+ */
 public class Biblioteka implements BibliotekaInterface {
 
+	/**
+	 * Lista sa svim knjigama u biblioteci
+	 */
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 	
 	
