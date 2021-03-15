2b743c0d3777d202dfb6720b85e4bc7ae5d66cdb
majaa12 <majacol98@gmail.com>
2021-03-15 15:44:03 +0100

    Dokumentovan interfejs BibliotekaInterface

--- src/biblioteka/Interfejs/BibliotekaInterface.java
+++ src/biblioteka/Interfejs/BibliotekaInterface.java
@@ -5,8 +5,22 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Interfejs koji predstavlja biblioteku.
+ * 
+ * @author Maja Colovic
+ *
+ */
 public interface BibliotekaInterface {
 
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 * 
+	 * @param knjiga Knjiga koja se dodaje.
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta knjiga null.
+	 * @throws java.lang.RuntimeException ako uneta knjiga vec postoji u biblioteci (duplikat)
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
 	
 	public void obrisiKnjigu(Knjiga knjiga);
